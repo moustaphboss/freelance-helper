e8938f75d59d09bc77b2af29c1c1377338ef21ec
Moustapha Kebe <moustaphakebe@192.168.1.26>
2023-10-23 20:53:56 +0000

    db file, removing the process.env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect('mongodb+srv://moustaphboss:Ahil7ZRkJiGnUELx@cluster0.iphcyqk.mongodb.net/freelance-helper?retryWrites=true&w=majority')
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(error.message);
